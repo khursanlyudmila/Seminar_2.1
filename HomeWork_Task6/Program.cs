78fce1359f54d1f7a430dd8df011004bc597756c
Khursan Lyudmila <khursanlyudmila@gmail.com>
2022-12-17 23:28:12 +0300

    add Home Task 6

---
+++ HomeWork_Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+
+string numberStr = Console.ReadLine() ?? "";
+int number = int.Parse(numberStr);
+if(number % 2 == 0)
+{
+    Console.WriteLine($"Число {number} является четным - да");
+}
+else
+{
+    Console.WriteLine($"Число {number} является четным - нет");
+}
