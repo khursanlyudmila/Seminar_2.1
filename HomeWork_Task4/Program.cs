cab360f1df83daf69d00b3f4014fd3094b851060
Khursan Lyudmila <khursanlyudmila@gmail.com>
2022-12-17 18:33:04 +0300

    add Home Work Task 4

---
+++ HomeWork_Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число 1:");
+
+string numberStr1 = Console.ReadLine() ?? "";
+int number1 = int.Parse(numberStr1);
+
+Console.WriteLine("Введите число 2:");
+
+string numberStr2 = Console.ReadLine() ?? "";
+int number2 = int.Parse(numberStr2);
+
+Console.WriteLine("Введите число 3:");
+
+string numberStr3 = Console.ReadLine() ?? "";
+int number3 = int.Parse(numberStr3);
+
+int result = number1;
+if(number2 > result) result = number2;
+if(number3 > result) result = number3;
+
+Console.WriteLine($"Наибольшее число из {number1}, {number2} и {number3} - число {result}");
