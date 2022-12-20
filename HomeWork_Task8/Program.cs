e19dd36d1e8d590bb14c5eecd20da9e7b3fed419
Khursan Lyudmila <khursanlyudmila@gmail.com>
2022-12-20 09:29:25 +0300

    add Home Work Task 8

---
+++ HomeWork_Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+
+int count = 1;
+
+while (count <= number)
+{
+    if(number % 2 == 0)
+    {
+        Console.Write($"{count+1}, ");
+    }
+    else
+        Console.Write($"{count-1}, ");
+    count += 2;
+}
